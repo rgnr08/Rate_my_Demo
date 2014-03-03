95f3f47b91c22be1c836773abc4bdf494c26b556
Granvanoeli <levent.jade@gmail.com>
2014-03-03 16:14:07 +0000

    updated admin.py and started creating login view

--- Rate_my_Demo/admin.py
+++ Rate_my_Demo/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from Rate_my_Demo.models import Genre, RateMyDemoUser, Demo
 
+# This thing here displays the categories in the admin page (sorta)
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('Genre', 'RateMyDemoUser', 'Demo')
 
 
 admin.site.register(Genre)
-admin.site.register(RateMyDemoUser)
 admin.site.register(Demo)
+admin.site.register(RateMyDemoUser)
--- Rate_my_Demo/forms.py
+++ Rate_my_Demo/forms.py
@@ -2,5 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from Rate_my_Demo.models import Demo, RateMyDemoUser
 
-class RateMyDmoUserForm(forms.ModelForm):
-    user = forms.CharField(User)
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class RateMyDemoUserForm(forms.ModelForm)
+    class Meta:
+        model = RateMyDemoUser
+        fields = ('thumbnail', 'favourite_genres', 'location', 'favourites', 'uploaded_demos', 'artist')
